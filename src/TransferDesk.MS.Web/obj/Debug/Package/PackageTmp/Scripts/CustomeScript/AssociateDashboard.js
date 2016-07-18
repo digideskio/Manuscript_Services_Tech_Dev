85b0bb5d5db9f9f226cf64a1beb2a30e283f72ca
salonirajguru <saloni.rajguru@crest.in>
2016-07-18 14:34:32 +0530

    Commit of daya changes

---
+++ src/TransferDesk.MS.Web/obj/Debug/Package/PackageTmp/Scripts/CustomeScript/AssociateDashboard.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {
+    $("#btnFetchId").click(function () {
+        var manuscriptsID = $("#manuscriptsID").val();
+        $.ajax(
+        {
+            method: "GET",
+            url: AppPath + 'AssociateDashboard/FetchJob',
+            contentType: "application/json; charset=utf-8",
+            success: function (returnValue) {
+                debugger;
+                if (returnValue.returnValue = "true") {
+                    alert(returnValue.message);
+                    if (returnValue.ManuscriptID = 0) {
+                        alert('Manuscript not found.');
+                        window.open('/Manuscript', "_blank");
+                    }
+                    else {
+                        window.open('/Manuscript/HomePage/' + returnValue.ManuscriptID, "_blank");
+                    }
+                    window.location.reload(true);
+                }
+                else {
+                    alert(returnValue.message);
+                }
+            },
+            error: function (xhr, exception) {
+            }
+        });
+    });
+
+});
