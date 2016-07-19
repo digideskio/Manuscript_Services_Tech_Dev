acd305db7052a9db8b292c0a0ac0f3e79b93ac6c
pawankapoor <pawan.kapoor@crest.in>
2016-07-19 15:15:59 +0530

    new api config in app start

--- src/TransferDesk.MS.Web/Global.asax.cs
+++ src/TransferDesk.MS.Web/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -35,6 +36,7 @@ namespace TransferDesk.MS.Web
                 stringBuilder = new StringBuilder();
 
                 AreaRegistration.RegisterAllAreas();
+                GlobalConfiguration.Configure(WebApiConfig.Register);
                 FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
                 RouteConfig.RegisterRoutes(RouteTable.Routes);
                 BundleConfig.RegisterBundles(BundleTable.Bundles);
