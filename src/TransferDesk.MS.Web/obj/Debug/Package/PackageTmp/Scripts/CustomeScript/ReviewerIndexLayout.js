7dcafc137d884207255ea97423081262dbabf971
samandhare <sambhaji.andhare@crest.in>
2016-07-22 16:28:50 +0530

    RI Link Issue

---
+++ src/TransferDesk.MS.Web/obj/Debug/Package/PackageTmp/Scripts/CustomeScript/ReviewerIndexLayout.js
@@ -0,0 +1,32 @@
+﻿function ShowSuccessMessage(message) {
+
+    $('#NotificationMessageControl').removeClass("InfoMessage");
+    $('#NotificationMessageControl').removeClass("ErrorMessage");
+    $('#NotificationMessageControl').addClass("SaveMessage");
+    $('#NotificationMessageControl').fadeIn('slow');
+    $('#NotificationMessage').html(message);
+    $('#NotificationSubMessage').hide();
+    $('#CloseButton').show();
+    $("#non-vendorCode").height("40%");
+    setTimeout(function () {
+        $('#NotificationMessageControl').fadeOut('slow', function () { });
+    }, 4000);
+}
+function ShowFailureResponseMessage(message) {
+
+    $('#NotificationMessageControl').removeClass("InfoMessage");
+    $('#NotificationMessageControl').removeClass("ErrorMessage");
+    $('#NotificationMessageControl').addClass("ErrorMessage");
+    $('#NotificationMessageControl').fadeIn('slow');
+    $('#NotificationMessage').html(message);
+    $('#NotificationSubMessage').hide();
+    $('#CloseButton').show();
+    $("#non-vendorCode").height("40%");
+    setTimeout(function () {
+        $('#NotificationMessageControl').fadeOut('slow', function () { });
+    }, 4000);
+}
+function HideNotification() {
+    $('#CloseButton').hide();
+    $("#NotificationMessageControl").hide();
+}
